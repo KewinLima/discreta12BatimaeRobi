d8984ac0e012aa7d6f604a2d3fb05494671c5254
Isabella Vitoria Galvao Dos Santos <isabella_asg@hotmail.com>
2015-12-04 23:16:17 -0300

    Cabeçalho

--- ex12.c
+++ ex12.c
@@ -1,3 +1,10 @@
+/* *******************************************************
+ **| Programa   : Rede de Petri com Processos Paralelos|**
+ **| Autores    : Isabella Galvão e Kewin Lima         |**
+ **| Orientador : Ruben Carlo                          |**
+ *********************************************************
+ **/
+
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
