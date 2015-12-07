9a75bf7f6f9a7c80388b774c9e49f0ef1f5f4b89
Kewin Lima Da Silva <lima_kewin@hotmail.com>
2015-12-07 18:25:00 -0300

    Inicionando fopne

--- ex12.c
+++ ex12.c
@@ -1,6 +1,31 @@
+#define NENTRADA 5;
 #include <stdio.h>
 
 int main(void)
 {
+    //q[0] = qunatidade de lugar, 
+    //q[1] = quantidade de trans,
+    //q[2] = quantidade de lugartoke,
+    //q[3] = qarcolugar,
+    //q[4] = qarcotrans,
+
+    int n;
+    
+    FILE *a = fopen("sena1450.txt","r"); // Abrindo o arquivo de entrada
+
+    for(n=0;n<NENTRADA;n++) //Laço para o vetor q
+    {
+        fscanf("%d", &q[n]);
+    }
+
+    int *lugares = cria_lugares(q[0]);
+
+    free(lugares); // limpando a alocação dinamica
+    fclose(a);// fechando o arquivo de entrada
     return 0;
+}
+
+int *cria_lugares(int numero_de_lugares)
+{
+    return malloc(numero_de_lugares * (sizeof int));
 }
